87269bce6351e497a95c43e30aef3a2fa72e4225
1538379200 <1538379200@qq.com>
2025-06-01 11:22:01 +0800

    添加tooltip组件

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="main-content">
+    <Tooltip content="asdasdasdasdasddasdasdasdasdasdasdasdasdasdasdasdasdasdasxsaasweasdas" position="right">
+      <button>TEST TOOLTIP</button>
+    </Tooltip>
+    <div>hello</div>
+    <div class="w-10"></div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup lang="ts">
+import Tooltip from './components/tooltip/Tooltip.vue';
+
+
+</script>
+
+<style lang="css">
+.main-content {
+  display: flex;
+  justify-content: center;
+  margin-top: 30em;
 }
 </style>
---
+++ src/components/tooltip/Tooltip.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="trigger">
+        <div :class="['tooltip', props.position]" :data-tooltip="props.content"></div>
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+import "./index.css";
+
+type TooltipPosition = "top" | "bottom" | "left" | "right"
+
+interface Props {
+    content: string,
+    position?: TooltipPosition
+}
+
+// const props = defineProps<Props>()
+
+const props = withDefaults(defineProps<Props>(), {
+    position: "top"
+})
+</script>
